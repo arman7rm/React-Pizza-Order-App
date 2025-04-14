cdf274e2364747bbc32c45f159de906a04582940
arman7rm <arman97rm@gmail.com>
2025-04-14 01:44:13 -0400

    updates

---
+++ src/Header.jsx
@@ -0,0 +1,14 @@
+import { CartContext } from "./contexts";
+import { useContext } from "react";
+
+export default function Header() {
+    const [cart] = useContext(CartContext);
+    return (
+      <nav>
+        <h1 className="logo">Padre Gino's Pizza</h1>
+        <div className="nav-cart">
+          🛒<span className="nav-cart-number">{cart.length}</span>
+        </div>
+      </nav>
+    );
+  }
